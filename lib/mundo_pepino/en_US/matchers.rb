12fb8a088bf5be8f09582f1034be9e85a1354cd3
Nando <fernando.garcia@the-cocktail.com>
2009-11-17 21:16:09 +0100

    Parentheses moved to the _i_visit_ matcher (to keep it with its "grouping only" surname ;)

--- lib/mundo_pepino/en_US/matchers.rb
+++ lib/mundo_pepino/en_US/matchers.rb
@@ -13,8 +13,8 @@ module MundoPepino
       def _which_
         '(?:which|that have as)'
       end
-      def _visit_
-        '?:that I am (?:in|on|at)|I visit|I go to'
+      def _i_visit_
+        '(?:that I am (?:in|on|at)|I visit|I go to)'
       end
     end
   end
